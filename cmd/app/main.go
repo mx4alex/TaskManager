a353fe73baec67e3d8eb95383fb9f1d155abdbfa
mx4alex <90920531+mx4alex@users.noreply.github.com>
2023-07-24 12:50:34 +0300

    Create main.go

---
+++ cmd/app/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	fnc "TaskManager/internal/functions"
+)
+
+func main() {
+	var cmd string
+
+	Tasks, err := fnc.ReadTasks()
+	if err != nil {
+		fmt.Println("Ошибка чтения данных о задачах:", err)
+		return
+	}
+
+	for {
+		fnc.CommandPrint()
+		fmt.Scanln(&cmd)
+
+		switch cmd {
+		case "Create":
+			fnc.CreateTask(&Tasks)
+		case "Read":
+			fnc.PrintTasks(&Tasks)
+		case "Update":
+			fnc.UpdateTask(&Tasks)
+		case "Mark":
+			fnc.MarkTask(&Tasks)
+		case "Delete":
+			fnc.DeleteTask(&Tasks)
+		case "Exit":
+			fmt.Println("Программа завершена.")
+			return
+		default:
+			fmt.Println("Недопустимое действие.")
+		}
+
+		fmt.Println()
+	}
+}
