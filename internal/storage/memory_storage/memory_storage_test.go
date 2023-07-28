36eebd47f93e61b5b3575290159c48acf164246b
mx4alex <mx4alex@gmail.com>
2023-07-28 13:46:19 +0300

    add table-driven-tests

--- internal/storage/memory_storage/memory_storage_test.go
+++ internal/storage/memory_storage/memory_storage_test.go
@@ -8,59 +8,85 @@ import (
 )
 
 func TestMemoryStorage(t *testing.T) {
-	t.Run("add data", func(t *testing.T) {
-		expectedData := []entity.Task {
-			entity.NewTask(1, "Task 1", false),
-			entity.NewTask(2, "Task 2", false),
-		}
-	
-		ms, _ := memory_storage.New()
-		ms.AddTask("Task 1")
-		ms.AddTask("Task 2")
-	
-		resultData, _ := ms.GetTasks()
-		assert.Equal(t, expectedData, resultData)
-	})
+	testCases := []struct {
+		name         string
+		actions      func(ms *memory_storage.MemoryTaskStorage) error
+		expectedData []entity.Task
+		expectedErr  bool
+	}{
+		{
+			name: "add data",
+			actions: func(ms *memory_storage.MemoryTaskStorage) error {
+				ms.AddTask("Task 1")
+				ms.AddTask("Task 2")
+				return nil
+			},
+			expectedData: []entity.Task {
+				entity.NewTask(1, "Task 1", false),
+				entity.NewTask(2, "Task 2", false),
+			},
+			expectedErr: false,
+		},
+		{
+			name: "delete data",
+			actions: func(ms *memory_storage.MemoryTaskStorage) error {
+				ms.AddTask("Task 1")
+				ms.AddTask("Task 2")
+				return ms.DeleteTask(1)
+			},
+			expectedData: []entity.Task {
+				entity.NewTask(2, "Task 2", false),
+			},
+			expectedErr: false,
+		},
+		{
+			name: "mark data",
+			actions: func(ms *memory_storage.MemoryTaskStorage) error {
+				ms.AddTask("Task 1")
+				return ms.MarkTask(1)
+			},
+			expectedData: []entity.Task {
+				entity.NewTask(1, "Task 1", true),
+			},
+			expectedErr: false,
+		},
+		{
+			name: "update data",
+			actions: func(ms *memory_storage.MemoryTaskStorage) error {
+				ms.AddTask("Task 1")
+				return ms.UpdateTask(1, "New Task")
+			},
+			expectedData: []entity.Task {
+				entity.NewTask(1, "New Task", false),
+			},
+			expectedErr: false,
+		},
+		{
+			name: "test error",
+			actions: func(ms *memory_storage.MemoryTaskStorage) error {
+				ms.AddTask("Task 1")
+				return ms.UpdateTask(2, "New Task")
+			},
+			expectedData: []entity.Task {
+				entity.NewTask(1, "Task 1", false),
+			},
+			expectedErr: true,
+		},
+	}
 
-	t.Run("delete data", func(t *testing.T){
-		expectedData := []entity.Task {
-			entity.NewTask(2, "Task 2", false),
-		}
-	
-		ms, _ := memory_storage.New()
-		ms.AddTask("Task 1")
-		ms.AddTask("Task 2")
-	
-		err := ms.DeleteTask(1)
-		assert.NoError(t, err)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ms, _ := memory_storage.New()
+			err := tc.actions(ms)
 
-		resultData, _ := ms.GetTasks()
-		assert.Equal(t, expectedData, resultData)
-	})
+			if tc.expectedErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
 
-	t.Run("update data", func(t *testing.T){
-		expectedData := []entity.Task {
-			entity.NewTask(1, "New Task", false),
-			entity.NewTask(2, "Task 2", false),
-		}
-	
-		ms, _ := memory_storage.New()
-		ms.AddTask("Task 1")
-		ms.AddTask("Task 2")
-	
-		err := ms.UpdateTask(1, "New Task")
-		assert.NoError(t, err)
-
-		resultData, _ := ms.GetTasks()
-		assert.Equal(t, expectedData, resultData)
-	})
-
-	t.Run("test error", func(t *testing.T){
-		ms, _ := memory_storage.New()
-		ms.AddTask("Task 1")
-	
-		err := ms.UpdateTask(2, "New Task")
-		assert.Error(t, err)
-	})
-
+			resultData, _ := ms.GetTasks()
+			assert.Equal(t, tc.expectedData, resultData)
+		})
+	}
 }
